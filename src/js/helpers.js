031e93f53e3cd0fd7f47769f4ff6a9940bc0bcb8
JovinLiu <liujovin@gmail.com>
2024-01-01 21:41:12 +0800

    20240101

---
+++ src/js/helpers.js
@@ -0,0 +1,9 @@
+/**
+ * About Forkify API
+ * See API document: https://forkify-api.herokuapp.com/v2
+ * Key for this application: KEY = 826c11a7-fa34-412a-b1fc-d3d2623e1239
+ * To request a recipe list based on keywords: https://forkify-api.herokuapp.com/api/v2/recipes?search=<keywords>
+ * To update user's recipe: https://forkify-api.herokuapp.com/api/v2/recipes&key=<insert your key>
+ * To request a certain recipe based on id: https://forkify-api.herokuapp.com/api/v2/recipes/<id>
+ * To delete a user's recipe bease on id: https://forkify-api.herokuapp.com/api/v2/recipes/<id>?key=<insert your key>
+ */
